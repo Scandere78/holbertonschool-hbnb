68833a5536d7a719940d075b27079ad1b254116d
my-holber <8762@holbertonstudents.com>
2024-10-21 22:36:28 +0300

    update models

--- HBNB_2/hbnb/app/models/amenity.py
+++ HBNB_2/hbnb/app/models/amenity.py
@@ -2,8 +2,6 @@ from models import BaseModel
 
 
 class Amenity(BaseModel):
-    def __init__(self, name, created_at, updated_at):
+    def __init__(self, name):
         super.__init__()
         self.name = name
-        self.created_at = created_at
-        self.updated_at = updated_at
