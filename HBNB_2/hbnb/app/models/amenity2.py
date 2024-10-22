9f8b3ea2d9cd013c680a752452658fe0f6bfe500
LDtheGHOST <luissmith@hotmail.fr>
2024-10-22 16:48:59 +0200

    beug.4.6

--- HBNB_2/hbnb/app/models/amenity2.py
+++ HBNB_2/hbnb/app/models/amenity2.py
@@ -1,1 +1,11 @@
+from app.models.base_model import BaseModel
+from app.models.base_model import User
+from app.models.base_model import Places
 
+class Amenity(BaseModel):
+    def __init__(self, name):
+        super.__init__()
+        self.name = name
+
+        if len(name) > 50:
+            raise ValueError("The equipment name cannot exceed 50 characters.")
