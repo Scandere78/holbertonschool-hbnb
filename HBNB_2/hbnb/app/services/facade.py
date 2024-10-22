6da6e62ca212767eee800408bee7ee71147b5ab1
LDtheGHOST <luissmith@hotmail.fr>
2024-10-22 17:01:33 +0200

    beug.4.7

--- HBNB_2/hbnb/app/services/facade.py
+++ HBNB_2/hbnb/app/services/facade.py
@@ -17,13 +17,24 @@ class HBnBFacade:
         return self.user_repo.get_by_attribute('email', email)
     
     def create_amenity(self, amenity_data):
-    pass
+        # Placeholder for logic to create an amenity
+        amenity = Amenity(**amenity_data, id=str(uuid.uuid4()))
+        Amenity.validate_request_data(amenity_data)
+        self.amenity_repo.add(amenity)
+        return amenity
 
     def get_amenity(self, amenity_id):
-    pass
+         # Placeholder for logic to retrieve an amenity by ID
+        return self.amenity_repo.get(amenity_id)
 
     def get_all_amenities(self):
-    pass
+        # Placeholder for logic to retrieve all amenities
+        return list(self.amenity_repo.get_all())
 
     def update_amenity(self, amenity_id, amenity_data):
-    pass
+         # Placeholder for logic to update an amenity
+        Amenity.validate_request_data(amenity_data)
+        obj = self.get_amenity(amenity_id)
+        if obj:
+            obj.update(amenity_data)
+        return obj
