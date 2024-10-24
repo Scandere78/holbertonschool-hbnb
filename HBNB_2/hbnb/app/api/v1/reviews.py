43546e998ae1b32d134eb4c0874c3c1c6ef4ca53
root <root@v2643320.hosted-by-vdsina.ru>
2024-10-24 17:24:24 +0300

    beug.4.5

--- HBNB_2/hbnb/app/api/v1/reviews.py
+++ HBNB_2/hbnb/app/api/v1/reviews.py
@@ -16,8 +16,11 @@ review_model = api.model('Review', {
 
 @api.route('/')
 class ReviewList(Resource):
+
     @api.expect(review_model)
+    
     @api.response(201, 'Review successfully created')
+
     @api.response(400, 'Invalid input data')
     def post(self):
         """Register a new review"""
@@ -44,8 +47,10 @@ class ReviewList(Resource):
         return [{'id': review.id, 'text': review.text, 'rating': review.rating} for review in list_of_reviews], 200
     
     @api.route('/<review_id>')
-class ReviewResource(Resource):
+    class ReviewResource(Resource):
+
     @api.response(200, 'Review details retrieved successfully')
+
     @api.response(404, 'Review not found')
     def get(self, review_id):
         """Get review details by ID"""
@@ -61,8 +66,11 @@ class ReviewResource(Resource):
         }, 200
 
     @api.expect(review_model)
+
     @api.response(200, 'Review updated successfully')
+
     @api.response(404, 'Review not found')
+    
     @api.response(400, 'Invalid input data')
     def put(self, review_id):
         """Update a review's information"""
@@ -79,6 +87,7 @@ class ReviewResource(Resource):
         return {"message": "Review updated successfully"}, 200
 
     @api.response(200, 'Review deleted successfully')
+
     @api.response(404, 'Review not found')
     def delete(self, review_id):
         """Delete a review"""
@@ -88,10 +97,12 @@ class ReviewResource(Resource):
             return {'error': 'Review not found'}, 404
         facade.delete_review(review_id)
         return {"message": "Review deleted successfully"}, 200
+    
+    @api.route('/places/<place_id>/reviews')
+    class PlaceReviewList(Resource):
 
-@api.route('/places/<place_id>/reviews')
-class PlaceReviewList(Resource):
     @api.response(200, 'List of reviews for the place retrieved successfully')
+    
     @api.response(404, 'Place not found')
     def get(self, place_id):
         """Get all reviews for a specific place"""
