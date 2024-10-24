4710cc44dbe90031c64bb56275baa02a8df0d1ec
root <root@v2643320.hosted-by-vdsina.ru>
2024-10-24 16:30:00 +0300

    beug.4.5

--- HBNB_2/hbnb/app/api/v1/places.py
+++ HBNB_2/hbnb/app/api/v1/places.py
@@ -1,65 +1,59 @@
 from flask_restx import Namespace, Resource, fields
 from app.services.facade import HBnBFacade
 
-api = Namespace('places', description='Place operations')
+api = Namespace('amenities', description='Amenity operations')
 
-# Define the models for related entities
-amenity_model = api.model('PlaceAmenity', {
-    'id': fields.String(description='Amenity ID'),
-    'name': fields.String(description='Name of the amenity')
-})
-
-user_model = api.model('PlaceUser', {
-    'id': fields.String(description='User ID'),
-    'first_name': fields.String(description='First name of the owner'),
-    'last_name': fields.String(description='Last name of the owner'),
-    'email': fields.String(description='Email of the owner')
-})
-
-# Define the place model for input validation and documentation
-place_model = api.model('Place', {
-    'title': fields.String(required=True, description='Title of the place'),
-    'description': fields.String(description='Description of the place'),
-    'price': fields.Float(required=True, description='Price per night'),
-    'latitude': fields.Float(required=True, description='Latitude of the place'),
-    'longitude': fields.Float(required=True, description='Longitude of the place'),
-    'owner_id': fields.String(required=True, description='ID of the owner'),
-    'owner': fields.Nested(user_model, description='Owner details'),
-    'amenities': fields.List(fields.String, required=True, description="List of amenities ID's")
+# Define the amenity model for input validation and documentation
+amenity_model = api.model('Amenity', {
+    'name': fields.String(required=True, description='Name of the amenity')
 })
 
 facade = HBnBFacade()
 
 @api.route('/')
-class PlaceList(Resource):
-    @api.expect(place_model)
-    @api.response(201, 'Place successfully created')
+class AmenityList(Resource):
+    @api.expect(amenity_model)
+    @api.response(201, 'Amenity successfully created')
     @api.response(400, 'Invalid input data')
     def post(self):
-        """Register a new place"""
-        # Placeholder for the logic to register a new place
-        pass
-
-    @api.response(200, 'List of places retrieved successfully')
+        """Register a new amenity"""
+        amenity_data =  api.payload
+        # Placeholder for the logic to register a new amenity
+        existing_amenity = facade.get_all_amenities()
+        for i in existing_amenity:
+            if i.name == amenity_data['name']:
+                return {'error': f'{amenity_data["name"]} already registered'}, 400
+        new_amenity = facade.create_amenity(amenity_data)
+        return {'id':new_amenity.id, 'name':new_amenity.name}, 201
+    
+    @api.response(200, 'List of amenities retrieved successfully')
     def get(self):
-        """Retrieve a list of all places"""
-        # Placeholder for logic to return a list of all places
-        pass
+        _list = []
+        all_amenity = facade.get_all_amenities()
+        for i in all_amenity:
+            return_all = {'id':i.id, 'name':i.name}
+            _list.append(return_all)
+        return _list
 
-@api.route('/<place_id>')
-class PlaceResource(Resource):
-    @api.response(200, 'Place details retrieved successfully')
-    @api.response(404, 'Place not found')
-    def get(self, place_id):
-        """Get place details by ID"""
-        # Placeholder for the logic to retrieve a place by ID, including associated owner and amenities
-        pass
 
-    @api.expect(place_model)
-    @api.response(200, 'Place updated successfully')
-    @api.response(404, 'Place not found')
+@api.route('/<amenity_id>')
+class AmenityResource(Resource):
+    @api.response(200, 'Amenity details retrieved successfully')
+    @api.response(404, 'Amenity not found')
+    def get(self, amenity_id):
+        amenity = facade.get_amenity(amenity_id)
+        return {'name':amenity.name}
+
+    @api.expect(amenity_model)
+    @api.response(200, 'Amenity updated successfully')
+    @api.response(404, 'Amenity not found')
     @api.response(400, 'Invalid input data')
-    def put(self, place_id):
-        """Update a place's information"""
-        # Placeholder for the logic to update a place by ID
-        pass
+    def put(self, amenity_id):
+        amenity_data =  api.payload
+
+        facade.update_amenity(amenity_id, amenity_data)
+    
+        check_amenity = facade.get_amenity(amenity_id)
+        if check_amenity:
+            return {'name':check_amenity.name}, 201
+        return(404, 'Amenity not found')
--- HBNB_2/hbnb/app/api/v1/reviews.py
+++ HBNB_2/hbnb/app/api/v1/reviews.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+"""Module reviews API endpoint
+"""
 from flask_restx import Namespace, Resource, fields
-from app.services.facade import HBnBFacade
+from app.services.facade import facade
 
 api = Namespace('reviews', description='Review operations')
 
@@ -11,8 +14,6 @@ review_model = api.model('Review', {
     'place_id': fields.String(required=True, description='ID of the place')
 })
 
-facade = HBnBFacade()
-
 @api.route('/')
 class ReviewList(Resource):
     @api.expect(review_model)
@@ -21,13 +22,26 @@ class ReviewList(Resource):
     def post(self):
         """Register a new review"""
         # Placeholder for the logic to register a new review
-        pass
+        review_data = api.payload
+
+        try:
+            new_review = facade.create_review(review_data)
+        except ValueError as error:
+            return {'error': f"Review not created: {str(error)}"}, 400
+
+        return {
+            'text': new_review.text,
+            'rating': new_review.rating,
+            'user_id': new_review.user_id,
+            'place_id': new_review.place_id
+        }, 201
 
     @api.response(200, 'List of reviews retrieved successfully')
     def get(self):
         """Retrieve a list of all reviews"""
         # Placeholder for logic to return a list of all reviews
-        pass
+        list_of_reviews = facade.get_all_reviews()
+        return [{'id': review.id, 'text': review.text, 'rating': review.rating} for review in list_of_reviews], 200
 
 @api.route('/<review_id>')
 class ReviewResource(Resource):
@@ -36,7 +50,10 @@ class ReviewResource(Resource):
     def get(self, review_id):
         """Get review details by ID"""
         # Placeholder for the logic to retrieve a review by ID
-        pass
+        review = facade.get_review(review_id)
+        if not review:
+            return {'error': 'Review not found'}, 404
+        return {'id': review_id, 'text': review.text, 'rating': review.rating, 'user_id': review.user_id, 'place_id': review.place_id}, 200
 
     @api.expect(review_model)
     @api.response(200, 'Review updated successfully')
@@ -45,14 +62,27 @@ class ReviewResource(Resource):
     def put(self, review_id):
         """Update a review's information"""
         # Placeholder for the logic to update a review by ID
-        pass
+        review_data = api.payload
+
+        try:
+            updated_review = facade.update_review(review_id, review_data)
+        except ValueError as error:
+            return {'error': 'Invalid input data'}, 400
+
+        if not updated_review:
+            return {'error': 'Review not found'}, 404
+        return {"message": "Review updated successfully"}, 200
 
     @api.response(200, 'Review deleted successfully')
     @api.response(404, 'Review not found')
     def delete(self, review_id):
         """Delete a review"""
         # Placeholder for the logic to delete a review
-        pass
+        review = facade.get_review(review_id)
+        if not review:
+            return {'error': 'Review not found'}, 404
+        facade.delete_review(review_id)
+        return {"message": "Review deleted successfully"}, 200
 
 @api.route('/places/<place_id>/reviews')
 class PlaceReviewList(Resource):
@@ -61,4 +91,5 @@ class PlaceReviewList(Resource):
     def get(self, place_id):
         """Get all reviews for a specific place"""
         # Placeholder for logic to return a list of reviews for a place
-        pass
+        list_of_reviews = facade.get_reviews_by_place(place_id)
+        return [{'id': review.id, 'text': review.text, 'rating': review.rating} for review in list_of_reviews], 200
