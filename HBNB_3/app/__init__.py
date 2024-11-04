f04c977058ad78d10a093fa22459b85db18d1f9a
ScandereT78 <scanderetejpro@gmail.com>
2024-11-04 15:13:32 +0100

    add the config in init

--- HBNB_3/app/__init__.py
+++ HBNB_3/app/__init__.py
@@ -4,8 +4,9 @@ from app.api.v1.users import api as users_ns
 from app.api.v1.amenities import api as amenities_ns
 from app.api.v1.reviews import api as reviews_ns
 from app.api.v1.places import api as places_ns
-def create_app():
+def create_app(config_class="config.DevelopmentConfig"):
     app = Flask(__name__)
+    app.config.from_object(config_class)
     api = Api(app, version='1.0', title='HBnB API', description='HBnB Application API')
 
     # Register the users namespace
